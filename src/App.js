7983073f00d4d058c41d4c6f8f56e343f0e34711
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2021-02-05 20:26:33 +0530

    added Budgety Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [transaction, setTransaction] = useState({
+    description: "",
+    amount: "",
+  });
+  const [list, setList] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
+  const [balance, setBalance] = useState("");
+  const [credit, setCredit] = useState(
+    JSON.parse(localStorage.getItem("credit"))
+  );
+  const [debit, setDebit] = useState(JSON.parse(localStorage.getItem("debit")));
+
+  const updateForm = (e) => {
+    setTransaction({
+      ...transaction,
+      [e.target.name]:
+        e.target.type === "number" ? parseInt(e.target.value) : e.target.value,
+    });
+  };
+
+  const getBalance = () => {
+    const amounts = list.map((i) => i.amount);
+    const money = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    setBalance(money);
+  };
+
+  useEffect(() => {
+    getBalance();
+    localStorage.setItem("list", JSON.stringify(list));
+    localStorage.setItem("credit", JSON.stringify(credit));
+    localStorage.setItem("debit", JSON.stringify(debit));
+  }, [list]);
+
+  const plusMinus = () => {
+    transaction.amount > 0
+      ? setCredit(credit + transaction.amount)
+      : setDebit(debit + transaction.amount);
+  };
+
+  const clearBudget = () => {
+    setList([]);
+    setCredit(null);
+    setDebit(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Budgety</h1>
+      <div className="container layout">
+        <div className="form">
+          <h2 className="subtitle">Enter an Item</h2>
+          <form
+            onSubmit={(e) => {
+              const { description, amount } = transaction;
+              if (!description && !amount) {
+                alert("Enter Valid things");
+                e.preventDefault();
+              } else {
+                e.preventDefault();
+                setList([transaction, ...list]);
+                plusMinus();
+                setTransaction({ description: "", amount: "" });
+              }
+            }}
+          >
+            <div>
+              <input
+                type="text"
+                className="input"
+                placeholder="Enter Transaction"
+                value={transaction.description}
+                name="description"
+                onChange={updateForm}
+              />
+            </div>
+            <input
+              type="number"
+              className="input"
+              placeholder="Enter Amonut"
+              name="amount"
+              value={transaction.amount}
+              onChange={updateForm}
+            />
+            <div>
+              <button type="submit" className="button is-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+          <div>
+            <button className="button is-danger" onClick={clearBudget}>
+              Clear Budget
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="totals">
+        <h2 className="subtitle">Current Balance:</h2>
+        <h4>₹{balance}</h4>
+        <div>
+          <h3 className="subtitle">Credit</h3>
+          <h4>₹{credit}</h4>
+        </div>
+        <div>
+          <h3 className="subtitle">Debit</h3>
+          <h4>₹{debit}</h4>
+        </div>
+      </div>
     </div>
   );
 }
